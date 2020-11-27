680eff4b67bd80fe96c0321f9cb0bd55770ac621
Vaishnav Anil <vaishnavanil7th@gmail.com>
2020-11-27 15:42:55 +0530

    Updated gitfetch

--- assets/js/gitfetch.js
+++ assets/js/gitfetch.js
@@ -1,19 +1,19 @@
 (
     function(global) {
     	function fetchDescription(repo,id) {
-    		$.ajax('https://api.github.com/repos/Vshnv/'+repo).done(function(json) { 
+    		$.ajax('https://api.github.com/repos/'+repo).done(function(json) { 
    				console.log("Loaded " + repo);
    				console.log(json);
    				$("#"+id+"-desc").text(json.description);
    				console.log("#"+repos[i]+"-desc");
 
    				$("."+id).click(function() {
-   					window.open("https://github.com/Vshnv/" + repo);
+   					window.open("https://github.com/" + repo);
 				});
 			});
     	}
         
-        var repos = {"PersistanceFramework":"pf", "web-designs":"wd", "DjinnLang":"djinn"}
+        var repos = {"Vshnv/PersistanceFramework":"pf", "ElaraLang/Elara":"el", "Vshnv/Cataphract":"ct"}
         for (i in repos) {
         	fetchDescription(i,repos[i]);
         }
